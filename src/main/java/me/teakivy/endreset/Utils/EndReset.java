c9e2a69a3889864963fd14876f589441329be7f1
TeakIvy <legocjones@gmail.com>
2022-01-14 21:26:40 -0500

    Fixed /endreset not resetting entities, just regions

--- src/main/java/me/teakivy/endreset/Utils/EndReset.java
+++ src/main/java/me/teakivy/endreset/Utils/EndReset.java
@@ -13,7 +13,7 @@ public class EndReset {
     static Main main = Main.getPlugin(Main.class);
 
     public static void resetEnd(CommandSender sender, String worldName) {
-        Bukkit.getWorld(worldName).save();
+        Objects.requireNonNull(Bukkit.getWorld(worldName)).save();
         for (Player player : Bukkit.getOnlinePlayers()) {
             if (player.getWorld().getName().equals(worldName)) {
                 Location spawn = player.getBedSpawnLocation();
@@ -23,7 +23,7 @@ public class EndReset {
         }
         if (Main.devMode) Main.logger.info("Removed players from " + worldName);
         for (
-        Chunk chunk : Bukkit.getWorld(worldName).getLoadedChunks()) {
+        Chunk chunk : Objects.requireNonNull(Bukkit.getWorld(worldName)).getLoadedChunks()) {
             chunk.unload();
         }
         if (Main.devMode) Main.logger.info("Unloaded all chunks in " + worldName);
@@ -31,18 +31,37 @@ public class EndReset {
         Bukkit.getServer().unloadWorld(worldName, true);
         if (Main.devMode) Main.logger.info("Unloaded " + worldName);
 
-        File dir = new File(new File(Main.getPlugin(Main.class).getServer().getWorldContainer(), worldName).toString() + "/DIM1/region");
+        File regionDir = new File(new File(Main.getPlugin(Main.class).getServer().getWorldContainer(), worldName).toString() + "/DIM1/region");
 
-        if (!dir.exists()) {
+        if (!regionDir.exists()) {
             if (sender != null) sender.sendMessage(ChatColor.RED + "The world " + worldName + " has an improper file scheme!");
             Main.logger.severe("The world " + worldName + " has an improper file scheme!");
             return;
         }
 
-        if (Main.devMode) Main.logger.info("Found World Folder " + worldName);
+        if (Main.devMode) Main.logger.info("Found Region Folder " + worldName);
 
-        for(File file1: dir.listFiles())
+        for(File file1: Objects.requireNonNull(regionDir.listFiles()))
                 if (!file1.isDirectory())
+                if (!main.getConfig().getStringList("regions-to-save").contains(file1.getName())) {
+                    file1.delete();
+                    if (Main.devMode) Main.logger.info("Deleted " + file1.getName());
+                } else {
+                    if (Main.devMode) Main.logger.info("Skipped " + file1.getName());
+                }
+
+        File entityDir = new File(new File(Main.getPlugin(Main.class).getServer().getWorldContainer(), worldName).toString() + "/DIM1/entities");
+
+        if (!entityDir.exists()) {
+            if (sender != null) sender.sendMessage(ChatColor.RED + "The world " + worldName + " has an improper file scheme!");
+            Main.logger.severe("The world " + worldName + " has an improper file scheme!");
+            return;
+        }
+
+        if (Main.devMode) Main.logger.info("Found Entity Folder " + worldName);
+
+        for(File file1: Objects.requireNonNull(entityDir.listFiles()))
+            if (!file1.isDirectory())
                 if (!main.getConfig().getStringList("regions-to-save").contains(file1.getName())) {
                     file1.delete();
                     if (Main.devMode) Main.logger.info("Deleted " + file1.getName());
